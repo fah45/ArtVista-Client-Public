a3f39035de74e2e7fca9cef7f982de8b27282a63
Your Name git config --global user.email you@example.com <you@example.com>
2024-04-29 23:08:26 +0600

    resolve card section

--- src/components/CraftCard.jsx
+++ src/components/CraftCard.jsx
@@ -1,7 +1,13 @@
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 
-const CraftCard = () => {
+const CraftCard = ({craft}) => {
+
+    const { Subcategory, item, Price, Rating, PhotoUrl, Customization, Stock, Processing, Username, email, Description} = craft;
+
+
+
     return (
         <div>
             <Helmet>
@@ -9,15 +15,15 @@ const CraftCard = () => {
             </Helmet>
             <div className="card card-compact  transition border-2  hover:scale-100 md:hover:scale-105 lg:hover:scale-105 border-opacity-30 border-blue-500 hover:border-blue-700  group hover:no-underline focus:no-underline bg-base-100 shadow-xl">
                 <figure className=" pt-3">
-                    {/* <img src={} alt="Shoes" className="rounded-xl w-[350px] h-[180px]" /> */}
+                    <img src={PhotoUrl} alt="shoes" className="rounded-xl w-[350px] h-[180px]" />
                 </figure>
                 <div className="card-body items-left text-left">
                     <h2 className="card-title">{}</h2>
 
-                    <p><span className='font-bold'>Area: </span>{}</p>
+                    <p><span className='font-bold'>Item Name: </span>{item}</p>
 
-                    <p><span className='font-bold'>Status: </span>{}</p>
-                    <p><span className='font-bold'>Price:</span>{}</p>
+                    <p><span className='font-bold'>Price: </span>{Price}</p>
+                    <p><span className='font-bold'>Processing Time:</span>{Processing}</p>
 
                     <div className="card-actions">
                         {/* <Link to={`/estate/${id}`} className="btn text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><button>View Property</button></Link> */}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,9 +15,11 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <h2 className="text-3xl">craft craft: {crafts.item}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
             {
                 crafts.map(craft => <CraftCard key={craft._id} craft={craft}></CraftCard>)
             }
+            </div>
             <About></About>
             <Contact></Contact>
             
